673570e0dcba677d536ce5f8acad0bea5d4d0e54
fibo <casati_gianluca@yahoo.it>
2014-04-12 12:56:48 +0200

    solved 2

--- solved/2.go
+++ solved/2.go
@@ -9,6 +9,24 @@ import "fmt"
 
 func main() {
 	sum := 0
+	swap := 0
+	i1 := 1
+	i2 := 2
+
+	// isEven values occur when isEven = 0, where isEven is modulo 3
+	isEven := 2
+
+	for i1 < 4000000 {
+		swap = i1
+		i1 = i2
+		i2 += swap
+
+		isEven += 1
+		isEven = isEven % 3
+		if isEven == 0 {
+			sum += i1
+		}
+	}
 
 	fmt.Println(sum)
 }
